6b45365403c50908ff6baea7987b646adc5733b0
Rodolfo Campos <camposer@gmail.com>
2014-10-13 15:42:26 +0200

    Versión inicial del xml2csv (copia de mkyong)

---
+++ xml2csv/src/main/java/springbatch/basico/processor/FilterReportProcessor.java
@@ -0,0 +1,19 @@
+package springbatch.basico.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import springbatch.basico.model.Report;
+
+public class FilterReportProcessor implements ItemProcessor<Report, Report> {
+
+	@Override
+	public Report process(Report item) throws Exception {
+
+		// filter object which age = 30
+		if (item.getAge() == 30) {
+			return null; // null = ignore this object
+		}
+		return item;
+	}
+
+}
